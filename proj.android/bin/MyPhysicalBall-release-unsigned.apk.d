ed148503e0c34bcdfd70cfccf1d752bdaa32cc45
newstmd <tangxin@qq.com>
2015-05-28 18:33:15 +0800

    2015-5-28-18

---
+++ proj.android/bin/MyPhysicalBall-release-unsigned.apk.d
@@ -0,0 +1,5 @@
+/Users/tangxin/Documents/CocosProjects/AndroidMyPhysicalBall/proj.android/bin/MyPhysicalBall-release-unsigned.apk : \
+/Users/tangxin/Documents/CocosProjects/AndroidMyPhysicalBall/proj.android/bin/MyPhysicalBall.ap_ \
+/Users/tangxin/Documents/CocosProjects/AndroidMyPhysicalBall/proj.android/bin/classes.dex \
+/Volumes/Tools/CocosTools/sdk/tools/support/annotations.jar \
+/Users/tangxin/Documents/CocosProjects/AndroidMyPhysicalBall/proj.android/libs/armeabi/libcocos2dcpp.so \
